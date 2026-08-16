b6875d93ecb45abf9241b4db23b3f1334884dfa7
RAJ MALPANI <92161985+CODER-Rxj11@users.noreply.github.com>
2026-08-16 23:57:57 +0530

    Update demovan.js

--- backend/src/routes/demovan.js
+++ backend/src/routes/demovan.js
@@ -1,5 +1,5 @@
 import { GALLERY_CATEGORIES } from "../galleryData.js";
 import { createCategoryRouter } from "./galleryHelper.js";
-
 const config = GALLERY_CATEGORIES.find((c) => c.category === "demovan");
 const router = createCategoryRouter(config);
+export default router;
